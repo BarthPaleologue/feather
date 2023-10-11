c7317df5529e24e49adafda6d3adc107ff213d60
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-10-11 11:11:07 +0200

    cleared main scene

--- main.cpp
+++ main.cpp
@@ -21,31 +21,8 @@ int main() {
     scene.setActiveCamera(&camera);
 
     PointLight light("sun");
+    light.transform()->setPosition(50, 50, 0);
     scene.addPointLight(&light);
-
-    PointLight light2("light2");
-    light2.setColor(1, 0, 0);
-    light2.transform()->setPosition(0.0f, 0.0f, 10.0f);
-    DebugLight::CreateDebugPointLightMesh(&light2, scene);
-    scene.addPointLight(&light2);
-
-    PointLight light3("light3");
-    light3.setColor(0, 1, 0);
-    light3.transform()->setPosition(0.0f, 0.0f, -10.0f);
-    DebugLight::CreateDebugPointLightMesh(&light3, scene);
-    scene.addPointLight(&light3);
-
-    PointLight light4("light4");
-    light4.setColor(0, 0, 1);
-    light4.transform()->setPosition(10.0f, 0.0f, 0.0f);
-    DebugLight::CreateDebugPointLightMesh(&light4, scene);
-    scene.addPointLight(&light4);
-
-    PointLight light5("light5");
-    light5.setColor(1, 1, 0);
-    light5.transform()->setPosition(0.0f, 10.0f, 0.0f);
-    DebugLight::CreateDebugPointLightMesh(&light5, scene);
-    scene.addPointLight(&light5);
 
     PostProcessing colorCorrection("./assets/shaders/colorCorrection", &engine);
     colorCorrection.onBeforeRenderObservable.add([&]() {
@@ -56,45 +33,15 @@ int main() {
         colorCorrection.shader()->setFloat("brightness", 0.0f);
     });
 
-    //PostProcessing postProcessing2("./assets/shaders/invertPostProcess", &engine);
     scene.addPostProcess(&colorCorrection);
-    //scene.addPostProcess(&postProcessing2);
 
     PhongMaterial sunMaterial;
-    Texture sunMap("assets/textures/sun.jpg");
-    sunMaterial.setAmbientTexture(&sunMap);
+    sunMaterial.setDiffuseColor(1.0, 1.0, 1.0);
 
-    PhongMaterial mercuryMat;
-    mercuryMat.setDiffuseTextureFromFile("./assets/textures/mercury.jpg");
-
-    PhongMaterial venusMat;
-    venusMat.setDiffuseTextureFromFile("./assets/textures/venus.jpg");
-
-    PhongMaterial earthMat;
-    earthMat.setDiffuseTextureFromFile("./assets/textures/earth.jpg");
-    earthMat.setAmbientColor(0, 0.02, 0);
-
-    PhongMaterial moonMat;
-    moonMat.setDiffuseTextureFromFile("./assets/textures/moon.jpg");
-    moonMat.setAmbientColor(0, 0, 0.02);
-
-    PhongMaterial marsMat;
-    marsMat.setDiffuseTextureFromFile("./assets/textures/mars.jpg");
-
-    CelestialBody sun("sun", 1, 100, 10, 0, &sunMaterial, scene);
-    CelestialBody mercury("mercury", 0.2, 1.6, 1.5, 5, &mercuryMat, scene);
-    CelestialBody venus("venus", 0.45, -0.1, 8, 7, &venusMat, scene);
-
-    CelestialBody earth("earth", 0.5, 10, 10, 10, &earthMat, scene);
-    earth.getMesh()->transform()->setRotationX(0.3);
-    CelestialBody moon("moon", 0.25, 5, 5, 2, &moonMat, scene);
-
-    CelestialBody mars("mars", 0.35, 10, 12, 13, &marsMat, scene);
-
-    CelestialBody *currentTarget = &sun;
-
-    Mesh *banana = MeshLoader::loadObj("./assets/models/banana.obj", scene);
-    banana->material()->setWireframe(true);
+    Mesh *sphere = MeshBuilder::makeSphere("sun", scene, 32);
+    sphere->transform()->setPosition(0, 5, 0);
+    sphere->setMaterial(&sunMaterial);
+    sphere->material()->setWireframe(true);
 
     Mesh *plane = MeshBuilder::makePlane("plane", scene, 64);
     plane->transform()->setPosition(0, -2, 0);
@@ -102,55 +49,12 @@ int main() {
     plane->material()->setWireframe(true);
     plane->material()->setBackFaceCullingEnabled(false);
 
-    engine.onKeyPressObservable.add([&currentTarget, &sun, &earth, &moon, &plane](int key) {
-        if (key == GLFW_KEY_S) {
-            currentTarget = &sun;
-        } else if (key == GLFW_KEY_E) {
-            currentTarget = &earth;
-        } else if (key == GLFW_KEY_M) {
-            currentTarget = &moon;
-        }
-
+    engine.onKeyPressObservable.add([&](int key) {
         if (key == GLFW_KEY_W) plane->material()->setWireframe(!plane->material()->wireframe());
     });
 
     scene.onBeforeRenderObservable.add([&]() {
         auto elapsedTime = engine.getElapsedTime();
-
-        mercury.update(elapsedTime);
-        venus.update(elapsedTime);
-        earth.update(elapsedTime);
-        moon.update(elapsedTime);
-        moon.getMesh()->transform()->translate(*earth.getMesh()->transform()->position());
-        mars.update(elapsedTime);
-
-        light2.transform()->setPosition(
-                10.0f * cosf(1.0f * elapsedTime), 10.0f * sinf(1.0f * elapsedTime), 0.0f
-        );
-
-        light3.transform()->setPosition(
-                10.0f * cosf(1.0f * elapsedTime), 0.0f, 10.0f * sinf(1.0f * elapsedTime)
-        );
-
-        light4.transform()->setPosition(
-                0.0f, 10.0f * cosf(1.0f * elapsedTime), 10.0f * sinf(1.0f * elapsedTime)
-        );
-        light4.setColor(
-                0.5f * cosf(1.0f * elapsedTime) + 0.5f,
-                0.5f * sinf(1.0f * elapsedTime) + 0.5f,
-                0.5f * cosf(1.0f * elapsedTime) + 0.5f
-        );
-
-        light5.transform()->setPosition(
-                10.0f * cosf(1.0f * elapsedTime), -12.0f * sinf(1.0f * elapsedTime), 0.0f
-        );
-        light5.setColor(
-                0.5f * sinf(1.0f * elapsedTime) + 0.5f,
-                0.5f * cosf(1.0f * elapsedTime) + 0.5f,
-                0.5f * cosf(1.0f * elapsedTime) + 0.5f
-        );
-
-        camera.setTarget(currentTarget->getMesh()->transform()->position());
 
         camera.update();
     });
