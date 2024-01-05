db974e699903cdc8032748f7bee934c4be60222f
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-01-05 17:57:46 +0100

    fixed triangle winding for icosphere

--- core/meshes/MeshBuilder.h
+++ core/meshes/MeshBuilder.h
@@ -396,27 +396,27 @@ public:
         };
 
         vertexData.indices = {
-                        0,  4,  1,
-                        0,  9,  4,
-                        9,  5,  4,
-                        4,  5,  8,
-                        4,  8,  1,
-                        8,  10, 1,
-                        8,  3,  10,
-                        5,  3,  8,
-                        5,  2,  3,
-                        2,  7,  3,
-                        7,  10, 3,
-                        7,  6,  10,
-                        7,  11, 6,
-                        11, 0,  6,
-                        0,  1,  6,
-                        6,  1,  10,
-                        9,  0,  11,
-                        9,  11, 2,
-                        9,  2,  5,
-                        7,  2,  11
-                };
+                4, 0, 1,
+                9, 0, 4,
+                5, 9, 4,
+                5, 4, 8,
+                8, 4, 1,
+                10, 8, 1,
+                3, 8, 10,
+                3, 5, 8,
+                2, 5, 3,
+                7, 2, 3,
+                10, 7, 3,
+                6, 7, 10,
+                11, 7, 6,
+                0, 11, 6,
+                1, 0, 6,
+                1, 6, 10,
+                0, 9, 11,
+                11, 9, 2,
+                2, 9, 5,
+                2, 7, 11
+        };
 
         vertexData.computeNormals();
 
@@ -525,9 +525,10 @@ public:
         return Mesh::FromVertexData(name, vertexData);
     }
 
-    static std::shared_ptr<Mesh> Simplify(const char *name, Mesh *original, unsigned int nbSimplifications, Scene &scene) {
+    static std::shared_ptr<Mesh>
+    Simplify(const char *name, Mesh *original, unsigned int nbSimplifications, Scene &scene) {
         auto simplifiedData = original->vertexData();
-        for(unsigned int i = 0; i < nbSimplifications; i++) {
+        for (unsigned int i = 0; i < nbSimplifications; i++) {
             simplifiedData = simplifiedData.simplify();
         }
         auto simplifiedMesh = Mesh::FromVertexData(name, simplifiedData);
