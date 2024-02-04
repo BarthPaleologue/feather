ad8ad0a67618d9a94dea5c38a481a8d3ea5fc2bf
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-02-04 13:02:02 +0100

    default material is now pink
    it hurts less my eyes

--- assets/shaders/default/fragment.glsl
+++ assets/shaders/default/fragment.glsl
@@ -8,12 +8,8 @@ in vec3 vNormalW;
 in vec2 vUV;
 
 void main() {
-    vec3 color = vec3(0.0);
-
-    // 10x10 checkerboard
-    if (mod(floor(vPosition.x * 10.0) + floor(vPosition.y * 10.0) + floor(vPosition.z * 10.0), 2.0) == 0.0) {
-        color = vec3(1.0);
-    }
+    // pink
+    vec3 color = vec3(1.0, 0.0, 1.0);
 
     frag_color = vec4(color, 1.0);
 }
